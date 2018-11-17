fdf7fda13601932f451e21a162cf8c810f5f4edf
Laura <laura.rubio.arboleda1@correo.icesi.edu.co>
2018-11-17 12:10:23 -0500

    Algortimo prim pecueca

--- ProyectoAED/src/collections/Graph.java
+++ ProyectoAED/src/collections/Graph.java
@@ -6,18 +6,18 @@ public class Graph<V, E> {
 
 	public static final int SIZE = 10001; // maximo numero de vértices
 	public static final double INFINITY = 1 << 30;
-	public static final int AMOUNT=25;
+	public static final int AMOUNT = 5;
 	private int[][] adjacentsMatrix;
 	private double[][] weightMatrix;
-	private HashMap<String,NodeGraph<V>> vertices;
+	private HashMap<String, NodeGraph<V>> vertices;
 	private ArrayList<Edge<V, E>> edges;
 
 	public Graph() {
-		vertices = new  HashMap<String,NodeGraph<V>> ();
-		edges =new ArrayList <Edge<V,E>>();
+		vertices = new HashMap<String, NodeGraph<V>>();
+		edges = new ArrayList<Edge<V, E>>();
 		adjacentsMatrix = new int[AMOUNT][AMOUNT];
-		weightMatrix=new double[AMOUNT][AMOUNT];
-		
+		weightMatrix = new double[AMOUNT][AMOUNT];
+
 		inicializeMatrix();
 	}
 
@@ -28,7 +28,6 @@ public class Graph<V, E> {
 	public void setAdjacentsMatrix(int[][] adjacentsMatrix) {
 		this.adjacentsMatrix = adjacentsMatrix;
 	}
-	
 
 	public double[][] getWeightMatrix() {
 		return weightMatrix;
@@ -38,36 +37,36 @@ public class Graph<V, E> {
 		this.weightMatrix = weightMatrix;
 	}
 
-	public  HashMap<String,NodeGraph<V>>  getVertices() {
+	public HashMap<String, NodeGraph<V>> getVertices() {
 		return vertices;
 	}
-	
 
-	public void setVertices( HashMap<String,NodeGraph<V>>  vertices) {
+	public void setVertices(HashMap<String, NodeGraph<V>> vertices) {
 		this.vertices = vertices;
 	}
 
-	public ArrayList <Edge<V,E>> getEdges() {
+	public ArrayList<Edge<V, E>> getEdges() {
 		return edges;
 	}
 
-	public void setEdges(ArrayList <Edge<V,E>> edges) {
+	public void setEdges(ArrayList<Edge<V, E>> edges) {
 		this.edges = edges;
 	}
 
 	public NodeGraph<V> searchVertexx(V value) {
-		boolean found= false;
-		 NodeGraph<V> searched = null;
-		for(int i=0; i<vertices.size()&&!found;i++) {
-			if(vertices.get(i).getValue()==value) {
-				searched=vertices.get(i);
-				found =true;
-				
+		boolean found = false;
+		NodeGraph<V> searched = null;
+		for (int i = 0; i < vertices.size() && !found; i++) {
+			if (vertices.get(i).getValue() == value) {
+				searched = vertices.get(i);
+				found = true;
+
 			}
-			
+
 		}
 		return searched;
 	}
+
 	public NodeGraph<V> searchVertex(String key) {
 		return vertices.get(key);
 	}
@@ -75,12 +74,12 @@ public class Graph<V, E> {
 	public void addVertex(String key, V newVertex) {
 		if (vertices.isEmpty()) { // Si la tabla esta vacia que la posicion en la matriz sea (0,0)
 			NodeGraph<V> vertex = new NodeGraph<V>(newVertex, 0);
-			vertices.put(key,vertex);
+			vertices.put(key, vertex);
 		} else {
 			NodeGraph<V> vertex = new NodeGraph<V>(newVertex, vertices.size()); // si esta con al menos
-																									// 1 que sea el
-																									// (size,size)
-			vertices.put(key,vertex);
+																				// 1 que sea el
+																				// (size,size)
+			vertices.put(key, vertex);
 		}
 
 	}
@@ -97,14 +96,17 @@ public class Graph<V, E> {
 
 		edges.add(edge1);
 		addToMatrix(origin.getPos(), end.getPos());
-		
+
 		int x = edge1.getOrigin().getPos();
 		int y = edge1.getEnd().getPos();
 
+//		System.out.println(x +" pos x");
+//		System.out.println(y +" pos y");
+//		System.out.println(edge1.getWeight() +" get Wsigth");
 		weightMatrix[x][y] = edge1.getWeight();
 		weightMatrix[y][x] = edge1.getWeight();
-	   origin.addAdjacent( end);
-	   end.addAdjacent( origin);
+		origin.addAdjacent(end);
+		end.addAdjacent(origin);
 
 	}
 //
@@ -124,6 +126,7 @@ public class Graph<V, E> {
 				adjacentsMatrix[i][j] = 0;
 			}
 		}
+		fillWeightMatrix();
 
 	}
 
@@ -135,7 +138,7 @@ public class Graph<V, E> {
 		} else
 			return false;
 	}
-	
+
 	public Double adjacentsWeight(NodeGraph<V> origin, NodeGraph<V> end) {
 		if (weightMatrix[origin.getPos()][end.getPos()] > 0) {
 			return weightMatrix[origin.getPos()][end.getPos()];
@@ -153,56 +156,54 @@ public class Graph<V, E> {
 	}
 
 	public double[] dijkstra(String key) {
-    	 NodeGraph<V> node = searchVertex(key);
-    	 Queue<NodeGraph<V>> queue=new  LinkedList<NodeGraph<V>> ();
-    	 double[] distance = new double[vertices.size()]; 
-    	 boolean[] visited = new boolean[vertices.size()]; 
-    	          
-       int i=0;
-    		 for (Map.Entry<String, NodeGraph<V>> entry : vertices.entrySet()) {
-    			 if(entry.getKey()==key) {
-        			 distance[i] = 0.0;  
-                     visited[i] = false;
-        		 }else {
-        			 distance[i] = INFINITY;  
-                     visited[i] = false;
-        		 }
-    			 i++;
-    		 }
-    		
-    		  
-         
-    	 
-    	 queue.offer(node);
-    	
-    	 while(!queue.isEmpty()) {
-    		 NodeGraph<V> actual = queue.peek();
-    		 queue.poll();
-    		 int posActual = actual.getPos();
+		NodeGraph<V> node = searchVertex(key);
+		Queue<NodeGraph<V>> queue = new LinkedList<NodeGraph<V>>();
+		double[] distance = new double[vertices.size()];
+		boolean[] visited = new boolean[vertices.size()];
+
+		int i = 0;
+		for (Map.Entry<String, NodeGraph<V>> entry : vertices.entrySet()) {
+			if (entry.getKey() == key) {
+				distance[i] = 0.0;
+				visited[i] = false;
+			} else {
+				distance[i] = INFINITY;
+				visited[i] = false;
+			}
+			i++;
+		}
+
+		queue.offer(node);
+
+		while (!queue.isEmpty()) {
+			NodeGraph<V> actual = queue.peek();
+			queue.poll();
+			int posActual = actual.getPos();
 //    		 if(visited[posActual]) {
 //    			 continue;
 //    			 visited[posActual]=true;
 //    		 }
-    		
-    		 visited[posActual]=true;
-    				 for(int j = 0; j <actual.getAdjList().size(); j++){
-    					 NodeGraph<V>  adyacent=actual.getAdjList().get(j);
-    		    	     double peso = adjacentsWeight(actual,adyacent); 
-    		    	     int posAdj =adyacent.getPos();
-    		    	    // if( !visited[ posAdj] ){  
-    		    	    	 if( distance[ posActual ] + peso < distance[ posAdj ] ){
-    	    		                distance[ posAdj ] = distance[ posActual ] + peso;
-    	    		                queue.offer(adyacent);
-    	    		                
-    				 //}
-    			 }
-    				           
-    	                
-    			 }
-    		            
-    		 }return distance;
-    		 
-    	 }
+
+			visited[posActual] = true;
+			for (int j = 0; j < actual.getAdjList().size(); j++) {
+				NodeGraph<V> adyacent = actual.getAdjList().get(j);
+				double peso = adjacentsWeight(actual, adyacent);
+				int posAdj = adyacent.getPos();
+				// if( !visited[ posAdj] ){
+				if (distance[posActual] + peso < distance[posAdj]) {
+					distance[posAdj] = distance[posActual] + peso;
+					queue.offer(adyacent);
+
+					// }
+				}
+
+			}
+
+		}
+		return distance;
+
+	}
+
 	public void fillWeightMatrix() {
 
 		for (int i = 0; i < adjacentsMatrix.length; i++) {
@@ -237,43 +238,105 @@ public class Graph<V, E> {
 		return matrix;
 
 	}
-	
-	public int minWeight( boolean [] visits){
-		
 
-		double min=INFINITY;
-		int key=-1;
-		
-		for (int i=0; i<edges.size();i++) {
-			Edge<V,E> entry= edges.get(i);
-			if(entry.getWeight()<min && visits[i]) {
+	public int minWeight(Double key[],boolean[] visits) {
+
+		double min = INFINITY;
+		int val = -1;
+
+		for (int i = 0; i < key.length; i++) {
+			
+			if (key[i] < min && !visits[i]) {
+
 				
-				min=entry.getWeight();
-				key=i;
+				min = key[i];
+				val = i;
 			}
 		}
-		return key;
-		
+		return val;
+
 	}
-	
-//	public Graph<V,E> prim() {
-//		
-//		ArrayList<Edge<V, E>> parent= new ArrayList<Edge<V, E>>();
-//		ArrayList<Edge<V, E>> key= new ArrayList<Edge<V, E>>();
-//		
-//		boolean []visits= new boolean[AMOUNT];
-//		
-//		for (int i = 0; i < visits.length; i++) {
-//			
-//		}
-//		
-//		
-//		
-//		
-//		
-//		
-//	}
-//	public static void main(String[] args) {
+
+	public Double[] primMTS(String keyVertex) {
+		
+		NodeGraph<V>node= searchVertex(keyVertex);
+		int key=node.getPos();
+
+       Double dist[]= new Double[AMOUNT];
+       int parent[]= new int[AMOUNT];
+		boolean[] visits = new boolean[AMOUNT];
+		for (int i = 0; i < visits.length; i++) {
+			dist[i]=INFINITY;
+			visits[i] = false;
+		}
+		
+		dist[key]=0.0;
+		parent[key]=-1;
+
+		for (int i = 0; i < visits.length-1; i++) {
+			
+			int u= minWeight(dist, visits);
+			visits[u]=true;
+			
+			for (int j = 0; j < visits.length; j++) {
+				
+				if(weightMatrix[u][j]!=0 && !visits[j]&& weightMatrix[u][j]<dist[j]) {
+					parent[j]=u;
+					System.out.println(j);
+					dist[j]=weightMatrix[u][j];
+				}
+			}
+			
+		}
+		
+
+		return dist;
+
+	} 
+
+	public void prim(int key, Edge<V, E> edge, ArrayList<Double> dist) {
+
+		dist.set(key, 0.0);
+
+	}
+	public static void main(String[] args) {
+		Graph<String, Double> grafo= new Graph<String, Double>();
+		String S ="S";
+		String C="C";
+		String N="N";
+		String D="D";
+		String A="A";
+		
+		Double edge1=12.0;
+		Double edge2=9.0;
+		Double edge3=13.0;
+		Double edge4=14.0;
+		Double edge5=7.0;
+		Double edge6=10.0;
+		Double edge7=8.0;
+		
+		grafo.addVertex("S", S);
+		grafo.addVertex("C", C);
+		grafo.addVertex("N", N);
+		grafo.addVertex("D", D);
+		grafo.addVertex("A", A);
+		
+		grafo.insertEdge(edge1, "0", "S", "C", edge1);
+		grafo.insertEdge(edge2, "1", "S", "D", edge2);
+		grafo.insertEdge(edge3, "2", "D", "C", edge3);
+		grafo.insertEdge(edge4, "3", "D", "A", edge4);
+		grafo.insertEdge(edge5, "4", "A", "C", edge5);
+		grafo.insertEdge(edge6, "5", "C", "N", edge6);
+		grafo.insertEdge(edge7, "6", "N", "A", edge7);
+		
+		Double[] lista=grafo.primMTS("C");
+		
+		for (int i = 0; i < lista.length; i++) {
+			
+			System.out.println(i+ " VERTICE SIGUIENTE " + lista[i] );
+			
+		}
+		
 //		Graph<Edificio, Double> grafo = new Graph<Edificio, Double>();
 //		Edificio A = new Edificio("A");
 //		Edificio B = new Edificio("B");
@@ -339,7 +402,7 @@ public class Graph<V, E> {
 //		
 //		
 //		
-//	}
+	}
 //	
 
 }
