dc048b37b22ab09ac7476ccb2937f6c3aaf9177d
User <User@Hp-245-G5-w10>
2018-11-23 21:38:47 -0500

    Dibujar cuadros y dibujar lineas crack

--- ProyectoAED/src/application/SampleController.java
+++ ProyectoAED/src/application/SampleController.java
@@ -8,9 +8,11 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.SVGPath;
 import modelo.Building;
 import modelo.Icesi;
 
@@ -40,43 +42,121 @@ public class SampleController {
 	
 	@FXML
     private Canvas canvas;
+	
+	 @FXML
+	    private Rectangle D;
 
+	    @FXML
+	    private Rectangle E;
+
+	    @FXML
+	    private Rectangle Saman;
+
+	    @FXML
+	    private Rectangle Biblioteca;
+
+	    @FXML
+	    private Rectangle B;
+
+	    @FXML
+	    private Rectangle Central;
+
+	    @FXML
+	    private Rectangle Auditorios;
+
+	    @FXML
+	    private Rectangle A;
+
+	    @FXML
+	    private Rectangle N;
+
+	    @FXML
+	    private Rectangle Coliseo2;
+
+	    @FXML
+	    private Rectangle Bienestar;
+
+	    @FXML
+	    private Rectangle G;
+
+	    
+	    private GraphicsContext gc;
 	private String origin;
 
 	private String end;
+	
+	private String originPoint;
+	
+	private String endPoint;
+	
+	private boolean isLine;
 	/**
 	 * True para inicio False para final
 	 */
 	private boolean path;
 
-	MouseEvent event;
-	EventHandler e;
+	
 
 	public void initialize() {
 		
-		
+		isLine=true;
 		path = true;
 		origin = "";
 		end = "";
 		icesi = new Icesi();
-//		verificateMap();
-//		coliseoClick();
-//		LClick();
-//		CClick();
+		verificateMap();
+		coliseo1Click();
+		LClick();
+		CClick();
+		AClick();
+		BClick();
+		EClick();
+		NClick();
+		samanClick();
+		coliseo2Click();
+		auditoriosClick();
+		bibliotecaClick();
+		bienestarClick();
+		centralClick();
+		GClick();
+		DClick();
 		canvas();
 		// darL();
 		
 	}
 	public void canvas() {
-		Image image = new Image(getClass().getResourceAsStream("./imagenes/mapa.png"));
+		//Image image = new Image(getClass().getResourceAsStream("./imagenes/mapa.png"));
 		
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		gc.drawImage(image, canvas.getLayoutX(), canvas.getLayoutY(), canvas.getWidth(), canvas.getHeight());
+		gc = canvas.getGraphicsContext2D();
+		gc.fillRect( canvas.getLayoutX(), canvas.getLayoutY(), 60, 60);
+		gc.fillRect( canvas.getLayoutX()+60, canvas.getLayoutY()+60, 60, 60);
+		
+		   
+		   // gc.appendSVGPath("M "+L.getLayoutX()+" "+L.getLayoutY()+" L "+A.getLayoutX()+" "+A.getLayoutY());
+		   
 	}
+	
+//	public void drawLines(double X1, double Y1, double X2, double Y2) {
+//		 gc.setStroke(Color.BLUE);
+//		 gc.setLineWidth(5.0);
+//		 gc.beginPath();
+//		 gc.appendSVGPath("M "+X1+" "+Y1+" L "+X2+" "+Y2);
+//		 gc.closePath();
+//		 gc.stroke();
+//	}
+	public void drawLines(String origin, String end) {
+		 gc.setStroke(Color.BLUE);
+		 gc.setLineWidth(5.0);
+		 gc.beginPath();
+		 gc.appendSVGPath("M "+origin+" L "+end);
+		 gc.closePath();
+		 gc.stroke();
+	}
+
 
 	public void verificateMap() {
 
-		map.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
@@ -90,15 +170,23 @@ public class SampleController {
 		});
 	}
 
-	public void origen(String building) {
-
+	public void origen(String building, double X, double Y) {
 		if (path) {
 			origin = building;
+			originPoint=X+" "+Y;
 			path = false;
-		} else
+		} else {
 			end = building;
+			endPoint=X+" "+Y;;
+		}
+		
 		if (origin != "" && end != "") {
-
+			if(originPoint != "" && endPoint != "") {
+				if(isLine) {
+			      drawLines(originPoint,endPoint);
+				}
+			
+			}
 			System.out.println("INICIO " + origin + " FIN " + end);
 		}
 
@@ -113,7 +201,7 @@ public class SampleController {
 		return building;
 	}
 
-	private void coliseoClick() {
+	private void coliseo1Click() {
 		// TODO Auto-generated method stub
 		Coliseo1.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
@@ -127,7 +215,86 @@ public class SampleController {
 				Coliseo1.setOpacity(1);
 
 				getBuilding(Coliseo1, "Coliseo1");
-				origen("Coliseo1");
+				origen("Coliseo1",Coliseo1.getLayoutX(),Coliseo1.getLayoutY());
+			}
+
+		});
+	}
+	private void coliseo2Click() {
+		// TODO Auto-generated method stub
+		Coliseo2.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				Coliseo2.setVisible(true);
+				Coliseo2.setOpacity(1);
+
+				getBuilding(Coliseo2, "Coliseo2");
+				origen("Coliseo2",Coliseo2.getLayoutX(),Coliseo2.getLayoutY());
+			}
+
+		});
+	}
+	
+	private void centralClick() {
+		// TODO Auto-generated method stub
+		Central.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				Central.setVisible(true);
+				Central.setOpacity(1);
+
+				getBuilding(Central, "Central");
+				origen("Central",Central.getLayoutX(),Central.getLayoutY());
+			}
+
+		});
+	}
+	
+	private void samanClick() {
+		// TODO Auto-generated method stub
+		Saman.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				Saman.setVisible(true);
+				Saman.setOpacity(1);
+
+				getBuilding(Saman, "Saman");
+				origen("Saman",Saman.getLayoutX(),Saman.getLayoutY());
+			}
+
+		});
+	}
+	
+	private void auditoriosClick() {
+		// TODO Auto-generated method stub
+		Auditorios.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				Auditorios.setVisible(true);
+				Auditorios.setOpacity(1);
+
+				getBuilding(Auditorios, "Auditorios");
+				origen("Auditorios",Auditorios.getLayoutX(),Auditorios.getLayoutY());
 			}
 
 		});
@@ -148,7 +315,7 @@ public class SampleController {
 				L.setOpacity(1);
 
 				lineCol1_L.setOpacity(1);
-				origen("L");
+				origen("L",L.getLayoutX(),L.getLayoutY());
 				getBuilding(L, "L");
 
 			}
@@ -171,8 +338,192 @@ public class SampleController {
 				C.setOpacity(1);
 
 				lineCol1_L.setOpacity(1);
-				origen("C");
+				origen("C",C.getLayoutX(),C.getLayoutY());
 				getBuilding(C, "C");
+
+			}
+
+		});
+	}
+	
+	private void AClick() {
+		// TODO Auto-generated method stub
+
+		A.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				A.setVisible(true);
+				A.setOpacity(1);
+
+				
+				origen("A",A.getLayoutX(),A.getLayoutY());
+				getBuilding(A, "A");
+
+			}
+
+		});
+	}
+	
+	private void BClick() {
+		// TODO Auto-generated method stub
+
+		B.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				B.setVisible(true);
+				B.setOpacity(1);
+
+				
+				origen("B",B.getLayoutX(),B.getLayoutY());
+				getBuilding(B, "B");
+
+			}
+
+		});
+	}
+	
+	private void DClick() {
+		// TODO Auto-generated method stub
+
+		D.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				D.setVisible(true);
+				D.setOpacity(1);
+
+				
+				origen("D",D.getLayoutX(),D.getLayoutY());
+				getBuilding(D, "D");
+
+			}
+
+		});
+	}
+	
+	private void EClick() {
+		// TODO Auto-generated method stub
+
+		E.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				E.setVisible(true);
+				E.setOpacity(1);
+
+				
+				origen("E",E.getLayoutX(),E.getLayoutY());
+				getBuilding(E, "E");
+
+			}
+
+		});
+	}
+	
+	private void GClick() {
+		// TODO Auto-generated method stub
+
+		G.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				G.setVisible(true);
+				G.setOpacity(1);
+
+				
+				origen("G",G.getLayoutX(),G.getLayoutY());
+				getBuilding(G, "G");
+
+			}
+
+		});
+	}
+	
+	private void NClick() {
+		// TODO Auto-generated method stub
+
+		N.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				N.setVisible(true);
+				N.setOpacity(1);
+
+				
+				origen("N",N.getLayoutX(),N.getLayoutY());
+				getBuilding(N, "N");
+
+			}
+
+		});
+	}
+	
+	private void bienestarClick() {
+		// TODO Auto-generated method stub
+
+		Bienestar.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				Bienestar.setVisible(true);
+				Bienestar.setOpacity(1);
+
+				
+				origen("Bienestar",Bienestar.getLayoutX(),Bienestar.getLayoutY());
+				getBuilding(Bienestar, "Bienestar");
+
+			}
+
+		});
+	}
+	
+	private void bibliotecaClick() {
+		// TODO Auto-generated method stub
+
+		Biblioteca.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				if (origin != "" && end != "") {
+					refresh();
+				}
+				Biblioteca.setVisible(true);
+				Biblioteca.setOpacity(1);
+
+				
+				origen("Biblioteca",Biblioteca.getLayoutX(),Biblioteca.getLayoutY());
+				getBuilding(Biblioteca, "Biblioteca");
 
 			}
 
@@ -183,11 +534,26 @@ public class SampleController {
 
 		lineCol1_L.setOpacity(0);
 		C.setOpacity(0);
+		A.setOpacity(0);
+		B.setOpacity(0);
+		D.setOpacity(0);
+		E.setOpacity(0);
+		N.setOpacity(0);
+		G.setOpacity(0);
 		Coliseo1.setOpacity(0);
+		Coliseo2.setOpacity(0);
+		Bienestar.setOpacity(0);
+		Auditorios.setOpacity(0);
+		Central.setOpacity(0);
+		Saman.setOpacity(0);
+		Biblioteca.setOpacity(0);
 		L.setOpacity(0);
 		path = true;
 		origin = "";
 		end = "";
+		originPoint="";
+		endPoint="";
+		gc.clearRect(canvas.getLayoutX(), canvas.getLayoutY(), canvas.getWidth(), canvas.getWidth());
 	}
 
 }
--- ProyectoAED/src/modelo/Icesi.java
+++ ProyectoAED/src/modelo/Icesi.java
@@ -19,7 +19,19 @@ public class Icesi {
 	public void addVertex() {
 		graph.addVertex("L", new Building("L"));
 		graph.addVertex("Coliseo1", new Building("Coliseo1"));
+		graph.addVertex("Coliseo2", new Building("Coliseo2"));
 		graph.addVertex("C", new Building("C"));
+		graph.addVertex("G", new Building("G"));
+		graph.addVertex("D", new Building("D"));
+		graph.addVertex("E", new Building("E"));
+		graph.addVertex("N", new Building("N"));
+		graph.addVertex("A", new Building("A"));
+		graph.addVertex("B", new Building("B"));
+		graph.addVertex("Bienestar", new Building("Bienestar"));
+		graph.addVertex("Saman", new Building("Saman"));
+		graph.addVertex("Central", new Building("Central"));
+		graph.addVertex("Biblioteca", new Building("Biblioteca"));
+		graph.addVertex("Auditorios", new Building("Auditorios"));
 	}
 	
 	
